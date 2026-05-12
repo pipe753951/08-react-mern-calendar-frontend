2bdefc9d2f21dc45ca83402610a1b070f8233463
David Puentes <pipe01112009@gmail.com>
2026-05-12 16:03:56 -0500

    Installing Bootstrap, removing unnecesary code and assets, renaming some code and modifying code to prepare project.

---
+++ src/CalendarApp.tsx
@@ -0,0 +1,10 @@
+const CalendarApp = function () {
+  return (
+    <div>
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button className="btn btn-primary">Primary button</button>
+    </div>
+  );
+};
+
+export default CalendarApp;
