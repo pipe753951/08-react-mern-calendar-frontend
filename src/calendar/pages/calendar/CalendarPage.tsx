cf0302120d5fb664dbed0ce063388104a655c8ec
David Puentes <pipe01112009@gmail.com>
2026-05-13 11:09:51 -0500

    Installing @types/react-big-calendar on NPM.

--- src/calendar/pages/calendar/CalendarPage.tsx
+++ src/calendar/pages/calendar/CalendarPage.tsx
@@ -1,3 +1,15 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "es-419": es4,
+};
+
 const CalendarPage = function () {
   return <></>;
 };
