14e34e50343d6fcb5dc1d50b0cfa1d0e52e39b91
David Puentes <pipe01112009@gmail.com>
2026-05-21 18:26:30 -0500

    Styling CalendarNavbar.

--- src/calendar/components/layout/CalendarNavbar.tsx
+++ src/calendar/components/layout/CalendarNavbar.tsx
@@ -1,18 +1,24 @@
-import { Calendar, DoorOpen } from "lucide-react";
+import { Calendar, DoorOpen, UserCircle2 } from "lucide-react";
 
 const CalendarNavbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark px-4">
+    <nav className="navbar px-4 border-bottom">
       <span className="navbar-brand d-flex gap-2 align-items-center">
         <Calendar />
-        <span>Usuario</span>
+        <span>Calendario</span>
       </span>
 
-      <button className="btn btn-danger">
-        <DoorOpen />
-        &nbsp;
-        <span>Salir</span>
-      </button>
+      <div className="d-flex gap-2 align-items-center">
+        <span className="d-inline-flex gap-1 align-items-center">
+          <UserCircle2 />
+          <span>Usuario</span>
+        </span>
+        <button className="btn btn-danger">
+          <DoorOpen />
+          &nbsp;
+          <span>Salir</span>
+        </button>
+      </div>
     </nav>
   );
 };
